c7329ca7f03312c1d448e2acf6b1903b6a1eb98d
Siv Earley <searley96@gmail.com>
2023-02-18 20:15:02 -0600

    completed tasks 1 and 2

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,51 @@
 console.log('***** Music Collection *****')
 
-collection = [];
+const collection = [];
+
+
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    console.log(album);
+    return collection.push(album);
+
+}
+addToCollection('Bad Behavior', 'Broncho', 2018);
+
+addToCollection('Blue', 'Joni Mitchell', 1971);
+addToCollection('Just Enough Hip to Be a Woman', 'Broncho', 2014);
+addToCollection('My Woman', 'Angel Olsen', 2016);
+addToCollection('Electric Warrior', 'T.Rex', 1971);
+addToCollection('Out of Blue', 'Electric Light Orchestra', 1977);
+
+//console.log(collection);
+
+// function showCollection([]) {
+//     console.log(collection.length);
+//     for (let i = 0; i < collection.length; i++) {
+//         console.log(album.title + 'by' + album.artist + 'published in' + album.yearPublished);
+
+//     }
+// }
+
+function showCollection(x) {
+    console.log(x.length);
+    for (let i = 0; i < x.length; i++) {
+        t = collection[i].title;
+        a = collection[i].artist;
+        y = collection[i].yearPublished;
+        console.log(t + ' by ' + a + ', published in ' + y);
+    }
+}
+
+showCollection(collection);
+
+
+
+
+
+
+
