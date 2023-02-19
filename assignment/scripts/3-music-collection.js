b55f010ef95ab4048b49ac856595a89abe395ff7
Siv Earley <searley96@gmail.com>
2023-02-19 16:56:29 -0600

    working on stretch goal 1

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -35,18 +35,59 @@ showCollection(collection);
 
 
 function findByArtist(artist) {
-    resultArray = [];
+    resultArray0 = [];
     for (let i = 0; i < collection.length; i++) {
         if (artist === collection[i].artist) {
-            resultArray.push(collection[i]);
+            resultArray0.push(collection[i]);
         }
     }
-    return resultArray;
+    return resultArray0;
 }
 
 console.log(findByArtist('Broncho'));
 console.log(findByArtist('Joni Mitchell'));
 console.log(findByArtist('Derek'));
 
+// function search(artist, year) {
+//     resultArray1 = [];
+//     if (artist === "" || year === "" || artist.length === 0 || year.length === 0) {
+//         return collection;
+//     for (let i = 0; i < collection.length; i++) {
+//         if (artist === collection[i].artist && year === collection[i].yearPublished) {
+//             resultArray1.push(collection[i]);
+//         }
+//     }
+//     return resultArray1;
+//     } else {
+//         return resultArray1;
+//     }
+// }
+   
 
+
+// console.log(search("Broncho", ))
+
+
+
+
+let artist =[]
+// let artist = "abc"
+let year = "rdx"
+console.log(artist === "" || year === "" || artist.length === 0 || year.length === 0)
+
+
+
+
+
+// console.log(search(,""));
+
+// Create a function called `search`. This function should:
+// - Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
+// ```
+// { artist: 'Ray Charles', year: 1957 }
+// ```
+// - The returned output from `search` should meet these requirements:
+//   - Return a new array of all items in the `collection` matching *all* of the search criteria.
+//   - If no results are found, return an empty array.
+//   - If there is no search object or an empty search object provided as input, then return all albums in the `collection`.
 
